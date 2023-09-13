f63aae6c43ffb49167fcc39ea164b534aa2ddb43
Cristian D. Biancotti <cdbiancotti@gmail.com>
2023-09-13 03:27:59 -0300

    Semana 8 Actividad 4

---
+++ Semana 8/ejercicio_4.py
@@ -0,0 +1,21 @@
+'''
+### Metiendo a la bolsa
+
+Con las funciones de los ejercicios anteriores de esta semana, separarlas en 2 o mas modulos y 
+generar un paquete que incluya estos modulos.
+
+nota Recorda que en los modulos deben ir solo codigo que sea de definicion y en caso de
+querer agregar codigo de ejecucion en alguno de los modulos debe agregarse dentro de la siguiente condicional
+
+if __name__ == '__main__':
+
+'''
+
+# Pasos: 
+#     1. generar los modulos con las funciones que queremos que contengan
+#     2. crear una carpeta que sera nuestro paquete
+#     3. crear el archivo __init__.py
+#     4. en el mismo lugar donde esta el paquete crear un archivo setup.py
+#     5. completar el setup.py con los datos vistos en las clases
+#     6. ejecutar con python desde la consola el archivo setup.py y pasarle como argumento sdist
+#     7. Luego de estos pasos deberia haberse generado una carpeta dist en la cual se va a encontrar el paquete que creamos
