72b470d4103ef8a5ba46c51cdc2641ad8836dae4
Cristian D. Biancotti <cdbiancotti@gmail.com>
2023-09-20 06:29:32 -0300

    Semana 11 Activadad 2

--- guiarepaso/inicio/urls.py
+++ guiarepaso/inicio/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from inicio.views import anio_de_nacimiento, bienvenida, inicio, crear_mascota, buscar_paleta, crear_paleta
 
 urlpatterns = [
-    path('', inicio),
+    path('', inicio, name='inicio'),
     path('bienvenida/', bienvenida),
     path('edad/<edad>/', anio_de_nacimiento),
     path('crear-mascota/<nombre>/<edad>/', crear_mascota),
-    path('paletas/', buscar_paleta),
-    path('paletas/crear/', crear_paleta),
+    path('paletas/', buscar_paleta, name='buscar_paleta'),
+    path('paletas/crear/', crear_paleta, name='crear_paleta'),
 ]
