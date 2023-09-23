8c3724d8ac07d7db28351dc2524d52019f5e96ba
Cristian D. Biancotti <cdbiancotti@gmail.com>
2023-09-22 23:05:17 -0300

    Semana 12 Activadad 3 - Punto 2

--- guiarepaso/cuentas/views.py
+++ guiarepaso/cuentas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as django_login, authenticate
+from cuentas.forms import UserRegisterForm
 
 
 def login(request):
@@ -15,3 +16,14 @@ def login(request):
     else:
         form = AuthenticationForm()
     return render(request, "cuentas/login.html", {"form": form})
+
+def registro(request):
+      if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                  username = form.cleaned_data['username']
+                  form.save()
+                  return render(request,"inicio/inicio.html" ,  {"mensaje":"Usuario Creado :)"})
+      else:
+            form = UserRegisterForm()     
+      return render(request,"cuentas/registro.html" , {"form":form})
